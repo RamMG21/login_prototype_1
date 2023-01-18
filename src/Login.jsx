d05d58ff47623411050ce07ca24aa6d58ddfd216
Ramses-Moreno <ram.ramses21sentinela@gmail.com>
2023-01-17 21:01:23 -0800

    Login prototype
    In this repository there is a login in reactJS, with integrated styling.

---
+++ src/Login.jsx
@@ -0,0 +1,11 @@
+export const Login = () => {
+    return(
+        <from>
+            <label for="email">email</label>
+            <input type="email" placeholder="@declesh.com" id="email"></input>
+
+            <label for="password">password</label>
+            <input type="password" placeholder="******" id="password"></input>
+        </from>
+    )
+}
