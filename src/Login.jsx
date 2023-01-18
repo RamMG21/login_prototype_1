de4cf1c3a0a47ba3568f553acac400228f63cfb8
Ramses-Moreno <ram.ramses21sentinela@gmail.com>
2023-01-17 22:10:05 -0800

    next step
    More spaces (no css yet)

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,28 @@
-export const Login = () => {
-    return(
-        <from>
-            <label for="email">email</label>
-            <input type="email" placeholder="@declesh.com" id="email"></input>
+import React , {useState} from "react";
 
-            <label for="password">password</label>
-            <input type="password" placeholder="******" id="password"></input>
+
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        console.log(email);
+
+    }
+
+    return(
+        <div className="auth-form-container">
+        <from onSubmit={handleSubmit}>
+            <label htmlFor="email">email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="@declesh.com" id="email"></input>
+        
+            <label htmlFor="password">password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="******" id="password"></input>
+            <button type="submit">Log In</button>
         </from>
+        <button onClick={() => props.onFormSwitch('register')}>Don't have an account? Register Here</button>
+        </div>
     )
 }
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,5 +1,31 @@
-export const Register = () => {
+import React, { useState} from "react";
+
+export const Register = (props) => {
+
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        console.log(email);
+
+    }
+
     return(
-        <>Register</>
+        <>
+        <from onSubmit={handleSubmit}>
+            <label htmlFor="name">Full Name</label>
+            <input value={name} name="name" id="name" placeholder="Full name" />
+            <label htmlFor="email">email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="@declesh.com" id="email"></input>
+        
+            <label htmlFor="password">password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="******" id="password"></input>
+            <button type="submit">Log In</button>
+        </from>
+        <button onClick={() => props.onFormSwitch('login')}>Already have an account? Login Here</button>
+        </>
     )
 }
